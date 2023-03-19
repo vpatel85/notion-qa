25ca2e97e8c050ac6af5169d905069e493079239
Vishal Patel <vpatel85@gmail.com>
2023-03-19 13:46:07 -0400

    update start copy

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "What's the vacation policy?", key="input")
     return input_text
 
 
